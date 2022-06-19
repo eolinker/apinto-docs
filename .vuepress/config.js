28579bf15450966596a7c1b1d1cbe2c24d643d2c
吴泽楷 <2112005011@mail2.gdut.edu.cn>
2022-06-19 10:07:46 +0800

    dashborad page

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -18,6 +18,9 @@ module.exports = {
         children:[
           {
             text:"Apinto",link: "/docs/index.md"
+          },
+          {
+            text:"Apinto Dashboard",link: "/docs/dashboard/index.md"
           }
         ],
       },
@@ -77,117 +80,117 @@ module.exports = {
 
 function getSideBar(){
   return {
-    '/': [
+    '/docs': [
       {
         text: '关于Apinto',
-        link: '/docs/index.md',
+        link: '/docs/apinto/index.md',
       },
       {
         text: '快速开始',
         collapsible: true,
         children: [
-          '/docs/quick/introduction.md', 
-          '/docs/quick/arrange.md', 
-          '/docs/quick/quick_start.md', 
-          '/docs/quick/quick_course.md'
+          '/docs/apinto/quick/introduction.md', 
+          '/docs/apinto/quick/arrange.md', 
+          '/docs/apinto/quick/quick_start.md', 
+          '/docs/apinto/quick/quick_course.md'
         ],
       },
       {
         text: '系统配置',
-        link: '/docs/system/index.md',
+        link: '/docs/apinto/system/index.md',
         collapsible: true,
-        children: ['/docs/system/error_log.md'],
+        children: ['/docs/apinto/system/error_log.md'],
       },
       {
         text: '路由',
         collapsible: true,
-        children: ['/docs/router/http.md'],
+        children: ['/docs/apinto/router/http.md'],
       },
       {
         text: '服务',
         collapsible: true,
-        children: ['/docs/service/http.md'],
+        children: ['/docs/apinto/service/http.md'],
       },
       {
         text: '负载均衡',
         collapsible: true,
-        children: ['/docs/upstream/http.md'],
+        children: ['/docs/apinto/upstream/http.md'],
       },
       {
         text: '服务发现',
-        link: '/docs/discovery/index.md',
+        link: '/docs/apinto/discovery/index.md',
         collapsible: true,
         children: [
-          '/docs/discovery/static.md', 
-          '/docs/discovery/consul.md', 
-          '/docs/discovery/eureka.md', 
-          '/docs/discovery/nacos.md'
+          '/docs/apinto/discovery/static.md', 
+          '/docs/apinto/discovery/consul.md', 
+          '/docs/apinto/discovery/eureka.md', 
+          '/docs/apinto/discovery/nacos.md'
         ],
       },
       {
         text: '访问鉴权',
-        link: '/docs/auth/index.md',
+        link: '/docs/apinto/auth/index.md',
         collapsible: true,
         children: [
-          '/docs/auth/aksk.md', 
-          '/docs/auth/apikey.md', 
-          '/docs/auth/basic.md', 
-          '/docs/auth/jwt.md'
+          '/docs/apinto/auth/aksk.md', 
+          '/docs/apinto/auth/apikey.md', 
+          '/docs/apinto/auth/basic.md', 
+          '/docs/apinto/auth/jwt.md'
         ],
       },
       {
         text: '输出器',
         collapsible: true,
-        children: ['/docs/outputer/file.md', '/docs/outputer/syslog.md', '/docs/outputer/kafka.md', '/docs/outputer/http.md', '/docs/outputer/nsq.md'],
+        children: ['/docs/apinto/outputer/file.md', '/docs/apinto/outputer/syslog.md', '/docs/apinto/outputer/kafka.md', '/docs/apinto/outputer/http.md', '/docs/apinto/outputer/nsq.md'],
       },
       {
         text: 'formatter',
-        link: '/docs/formatter/index.md',
+        link: '/docs/apinto/formatter/index.md',
         collapsible: true,
         children: [
-        '/docs/formatter/line.md', 
-        '/docs/formatter/json.md', 
-        '/docs/formatter/system_valid.md'
+        '/docs/apinto/formatter/line.md', 
+        '/docs/apinto/formatter/json.md', 
+        '/docs/apinto/formatter/system_valid.md'
       ],
       },
 
       {
         text: '插件系统',
-        link: '/docs/plugins/index.md',
+        link: '/docs/apinto/plugins/index.md',
         collapsible: true,
         children: [
-          '/docs/plugins/plugin_build.md', 
-          '/docs/plugins/extra_params.md', 
-          '/docs/plugins/params_transformer.md', 
-          '/docs/plugins/proxy_rewrite.md', 
-          '/docs/plugins/ip_restriction.md', 
-          '/docs/plugins/rate_limiting.md', 
-          '/docs/plugins/auth.md', 
-          '/docs/plugins/response_rewrite.md', 
-          '/docs/plugins/circuit_breaker.md',
-          '/docs/plugins/cors.md',
-          '/docs/plugins/gzip.md',
-          '/docs/plugins/access_log.md',
+          '/docs/apinto/plugins/plugin_build.md', 
+          '/docs/apinto/plugins/extra_params.md', 
+          '/docs/apinto/plugins/params_transformer.md', 
+          '/docs/apinto/plugins/proxy_rewrite.md', 
+          '/docs/apinto/plugins/ip_restriction.md', 
+          '/docs/apinto/plugins/rate_limiting.md', 
+          '/docs/apinto/plugins/auth.md', 
+          '/docs/apinto/plugins/response_rewrite.md', 
+          '/docs/apinto/plugins/circuit_breaker.md',
+          '/docs/apinto/plugins/cors.md',
+          '/docs/apinto/plugins/gzip.md',
+          '/docs/apinto/plugins/access_log.md',
           
         ],
       },
       {
         text: 'cli命令',
-        link: '/docs/cli/index.md',
+        link: '/docs/apinto/cli/index.md',
         collapsible: true,
         children: [
-          '/docs/cli/start.md', 
-          '/docs/cli/stop.md', 
-          '/docs/cli/restart.md', 
-          '/docs/cli/join.md', 
-          '/docs/cli/leave.md', 
-          '/docs/cli/info.md', 
-          '/docs/cli/extender_install.md', 
-          '/docs/cli/extender_upgrade.md', 
-          '/docs/cli/extender_uninstall.md',
-          '/docs/cli/extender_download.md',
-          '/docs/cli/extender_info.md',
-          '/docs/cli/extender_version.md',
+          '/docs/apinto/cli/start.md', 
+          '/docs/apinto/cli/stop.md', 
+          '/docs/apinto/cli/restart.md', 
+          '/docs/apinto/cli/join.md', 
+          '/docs/apinto/cli/leave.md', 
+          '/docs/apinto/cli/info.md', 
+          '/docs/apinto/cli/extender_install.md', 
+          '/docs/apinto/cli/extender_upgrade.md', 
+          '/docs/apinto/cli/extender_uninstall.md',
+          '/docs/apinto/cli/extender_download.md',
+          '/docs/apinto/cli/extender_info.md',
+          '/docs/apinto/cli/extender_version.md',
         ],
       },
       {
@@ -195,5 +198,11 @@ function getSideBar(){
         link: '/view/open.md',
       },
     ],
+    "/docs/dashboard":[
+      {
+        text: '关于Apinto dashboard',
+        link: '/docs/dashboard/index.md',
+      },
+    ]
   }
 }
